afd479cbc945ba8ca4568d86017ee47f97c06def
ChristofferAAU <cn19@student.aau.dk>
2022-12-01 12:57:45 +0100

    Updated admin so something is displayed when rendered

---
+++ src/Admin.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+class Admin extends React.Component {
+
+    render (){
+        return (<>
+        <p>Admin page</p>
+        </>)
+    }
+}
+
+export default Admin;
