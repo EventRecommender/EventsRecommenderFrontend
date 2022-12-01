c94134340a0da5fa0ee234b16bb7ef8b14b7d56a
ChristofferAAU <cn19@student.aau.dk>
2022-12-01 12:58:17 +0100

    Updated to display something when rendered

---
+++ src/Organizer.js
@@ -0,0 +1,12 @@
+import React from "react"
+
+class Organizer extends React.Component {
+
+    render() {
+        return (<>
+        <p>Organizer page</p>
+        </>)
+    }
+}
+
+export default Organizer;
