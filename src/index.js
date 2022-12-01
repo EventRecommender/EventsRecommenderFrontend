c1eb98ee51c9f879d42e6d1444fb3ccd02baac36
ChristofferAAU <cn19@student.aau.dk>
2022-12-01 10:33:26 +0100

    Changed index to use login instead of student.

--- src/index.js
+++ src/index.js
@@ -2,12 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Student from './Student';
+import Login from './Loginpage';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Student />
+    <Login />
   </React.StrictMode>
 );
 
