4af3d3e996ba3f5a382ef14ca84661164a6eb03e
ChristofferAAU <cn19@student.aau.dk>
2022-12-02 11:09:43 +0100

    Refactored Organizer to be a function component

--- src/Organizer.js
+++ src/Organizer.js
@@ -1,12 +1,38 @@
-import React from "react"
+import React, { useState } from "react";
+import { Button, ButtonGroup } from "@material-ui/core";
+import styled from "styled-components";
 
-class Organizer extends React.Component {
+export default function Organizer() {
+    const [active, setActive] = useState(types[0])
+    return (
+      <>
+      <div class="App-rightalign">
+        <h5>Username goes here</h5>
+      </div>
+      <div style={{ display: "block", padding: 30 }}>
+        <h4>Logo goes here</h4>
+      </div>
+      <div>
+        <ButtonGroup className="App-ButtonGroup">
+          {types.map(type => (<ButtonToggle key={type} active={active === type} onClick={() => setActive(type)}>{type}</ButtonToggle>))}
+        </ButtonGroup>
+        <div>
+          <h1>Activities goes here</h1>
+        </div>
+      </div>
+      </>
+    );
+  };
 
-    render() {
-        return (<>
-        <p>Organizer page</p>
-        </>)
-    }
-}
+  const types = ["Created Events", "Create Events"];
 
-export default Organizer;
+  const ButtonToggle = styled(Button)`
+  float: center;
+  width: 50%;
+  opacity: 0.4;
+  ${({ active }) =>
+    active &&
+    `
+    opacity: 1;
+  `}
+`;
