fb1892f5e57c3cef562801b258c5f9f792822aa9
ChristofferAAU <cn19@student.aau.dk>
2022-12-05 13:15:19 +0100

    Made admin, so that it functions like Student

--- src/Admin.js
+++ src/Admin.js
@@ -1,13 +1,52 @@
-import React from "react";
+import React, {useState} from "react";
+import { Button, ButtonGroup } from "@material-ui/core";
+import styled from "styled-components";
+import Userlist from "Userlist.js";
+import Activitylist from "Activitylist.js";
 import './Admin.css';
 
-class Admin extends React.Component {
+function Admin({id}) {
 
-    render (){
-        return (<>
-        <p>Admin page</p>
-        </>)
-    }
-}
+    const [active, setActive] = useState(types[0])
+
+    let content;
+
+    if(active === types[0]){
+        content = <Userlist/>
+    }else if(active === types[1]){
+        content = <Activitylist />;
+    };
+    return (
+      <>
+      <div class="App-rightalign">
+        <h5>Username goes here</h5>
+      </div>
+      <div style={{ display: "block", padding: 30 }}>
+        <h4>Logo goes here</h4>
+      </div>
+      <div>
+        <ButtonGroup className="App-ButtonGroup">
+          {types.map(type => (<ButtonToggle key={type} active={active === type} onClick={() => setActive(type)}>{type}</ButtonToggle>))}
+        </ButtonGroup>
+        <div>
+          {content}
+        </div>
+      </div>
+      </>
+    );
+  };
+
+  const types = ["Users", "Activities"];
+
+  const ButtonToggle = styled(Button)`
+  float: center;
+  width: 50%;
+  opacity: 0.4;
+  ${({ active }) =>
+    active &&
+    `
+    opacity: 1;
+  `}`;
+
 
 export default Admin;
