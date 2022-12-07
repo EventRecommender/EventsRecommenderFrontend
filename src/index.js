0ab3df27742aafbc3f8dc60960d4a8751046a372
Frederik Rasmussen <frasm19@student.aau.dk>
2022-12-07 10:28:39 +0100

    cleanup and corrections

--- src/index.js
+++ src/index.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+
 import Login from './Loginpage';
-import reportWebVitals from './reportWebVitals';
-import App from './App';
-import Calendar from './Components/Calendar';
+
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Calendar />
+    <Login />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
