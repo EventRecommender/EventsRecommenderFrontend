31176324592e3d784ee8551d001ca7057ea3f2dd
Frederik Rasmussen <58684446+dunkedolmer@users.noreply.github.com>
2022-12-19 23:18:42 +0100

    modified getrecommendations endpoint

--- src/Services/APIService.js
+++ src/Services/APIService.js
@@ -16,7 +16,7 @@ class APIService
 
             for (let act of content)
             {
-                let event = 
+                let event =
                 {
                     key: act.id,
                     img: act.img,
@@ -35,18 +35,37 @@ class APIService
         localStorage.setItem('incomingActivities', JSON.stringify([]));
     }
 
-    getRecommendations()
+    async getRecommendations()
     {
-        fetch('/getRecommendations' + new URLSearchParams({ id: UserService.getID() }))
-            .then(res => res.json())
-            .then((result) => 
+        let url = new URL('/getRecommendations', window.location.origin),
+            params = { userid: UserService.getID() }
+        Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
+
+        const response = await fetch(url);
+        if (response.ok)
+        {
+            const content = await response.json()
+            let result = []
+
+            for (let act of content)
             {
-                localStorage.setItem('recommendations', JSON.stringify(result))
-                console.log(result);
-            }, (error) => 
-            {
-                localStorage.setItem('recommendations', JSON.stringify([]));
-            })
+                let event = 
+                {
+                    key: act.id,
+                    img: act.img,
+                    title: act.title,
+                    place: act.place,
+                    host: act.host,
+                    date: act.date,
+                    type: act.type,
+                    url: act.url,
+                    description: act.description
+                }
+                result.push(event);
+            }
+            localStorage.setItem('recommendations', JSON.stringify(result));
+        }
+        localStorage.setItem('recommendations', JSON.stringify([]));
     }
 }
 
